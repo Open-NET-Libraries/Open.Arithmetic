d2e50fc0a941fe2c06ef0ecc4b531bbb1b5b7c9f
Oren (electricessence) <oren@electrified.net>
2018-06-03 12:31:03 -0700

    Updates to Triangular math.

--- Triangular.cs
+++ Triangular.cs
@@ -6,53 +6,175 @@
 using Open.Collections;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
-using System.Threading;
 
 namespace Open.Arithmetic
 {
 
 	public static class Triangular
 	{
+		public static readonly short MaxInt16 = Reverse(short.MaxValue);
+		public static readonly ushort MaxUInt16 = Reverse(ushort.MaxValue);
+		public static readonly int MaxInt32 = Reverse(int.MaxValue);
+		public static readonly uint MaxUInt32 = Reverse(uint.MaxValue);
+		public static readonly long MaxInt64 = Reverse(long.MaxValue);
+		public static readonly ulong MaxUInt64 = Reverse(ulong.MaxValue);
 
-		public static uint Forward(uint n)
+		/// <summary>
+		/// Returns the total count of triangular numbers bound by n.
+		/// </summary>
+		/// <exception cref="ArgumentOutOfRangeException">Must be at least zero.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">Result will exceed maximum value for a 32 bit integer.</exception>
+		public static int ForwardInt(int n)
 		{
+			if (n < 0) throw new ArgumentOutOfRangeException(nameof(n), n, "Must be at least zero.");
+			if (n > MaxInt32) throw new ArgumentOutOfRangeException(nameof(n), n, "Result will exceed maximum value for a 32 bit integer.");
+			Contract.EndContractBlock();
+
+			return (int)Forward(n);
+		}
+
+		/// <summary>
+		/// Returns the total count of triangular numbers bound by n.
+		/// </summary>
+		/// <exception cref="ArgumentOutOfRangeException">Must be at least zero.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">Result will exceed maximum value for an unsigned 32 bit integer.</exception>
+		public static uint ForwardInt(uint n)
+		{
+			if (n < 0) throw new ArgumentOutOfRangeException(nameof(n), n, "Must be at least zero.");
+			if (n > MaxUInt32) throw new ArgumentOutOfRangeException(nameof(n), n, "Result will exceed maximum value for an unsigned 32 bit integer.");
+			Contract.EndContractBlock();
+
+			return (uint)Forward(n);
+		}
+
+		/// <summary>
+		/// Returns the total count of triangular numbers bound by n.
+		/// </summary>
+		public static ulong Forward(long n)
+		{
+			if (n < 0) throw new ArgumentOutOfRangeException(nameof(n), n, "Must be at least zero.");
+			Contract.EndContractBlock();
+
+			return Forward((ulong)n);
+		}
+
+		/// <summary>
+		/// Returns the total count of triangular numbers bound by n.
+		/// </summary>
+		/// <exception cref="ArgumentOutOfRangeException">Result will exceed maximum value for an unsigned 64 bit integer.</exception>
+		public static ulong Forward(ulong n)
+		{
+			if (n > MaxUInt64) throw new ArgumentOutOfRangeException(nameof(n), n, "Result will exceed maximum value for an unsigned 64 bit integer.");
+			Contract.EndContractBlock();
+
 			return n * (n + 1) / 2;
 		}
 
+		static double ReverseInternal(double n)
+			=> (Math.Sqrt(8 * n + 1) - 1) / 2;
 
-		public static uint Reverse(uint n)
+		/// <summary>
+		/// Returns the source value (possible decimal) from an index in a triangular set.
+		/// </summary>
+		/// <exception cref="ArgumentOutOfRangeException">Cannot be NaN.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">Must be finite.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">Must be at least zero.</exception>
+		public static double Reverse(double n)
 		{
-			return (uint)(Math.Sqrt(8 * n + 1) - 1) / 2;
+			if (double.IsNaN(n)) throw new ArgumentOutOfRangeException(nameof(n), n, "Cannot be NaN.");
+			if (double.IsInfinity(n)) throw new ArgumentOutOfRangeException(nameof(n), n, "Must be finite.");
+			if (n < 0) throw new ArgumentOutOfRangeException(nameof(n), n, "Must be at least zero.");
+			Contract.EndContractBlock();
+
+			return ReverseInternal(n);
+		}
+
+		/// <summary>
+		/// Returns the source index from an index in a triangular set.
+		/// </summary>
+		/// <exception cref="ArgumentOutOfRangeException">Must be at least zero.</exception>
+		public static ulong Reverse(ulong n)
+			=> (ulong)ReverseInternal(n);
+
+		/// <summary>
+		/// Returns the source index from an index in a triangular set.
+		/// </summary>
+		public static uint Reverse(uint n)
+			=> (uint)ReverseInternal(n);
+
+		/// <summary>
+		/// Returns the source index from an index in a triangular set.
+		/// </summary>
+		public static short Reverse(short n)
+			=> (short)ReverseInternal(n);
+
+		/// <summary>
+		/// Returns the source index from an index in a triangular set.
+		/// </summary>
+		public static ushort Reverse(ushort n)
+			=> (ushort)ReverseInternal(n);
+
+		/// <summary>
+		/// Returns the source index from an index in a triangular set.
+		/// </summary>
+		/// <exception cref="ArgumentOutOfRangeException">Must be at least zero.</exception>
+		public static long Reverse(long n)
+		{
+			if (n < 0) throw new ArgumentOutOfRangeException(nameof(n), n, "Must be at least zero.");
+			Contract.EndContractBlock();
+
+			return (long)ReverseInternal(n);
+		}
+
+		/// <summary>
+		/// Returns the source index from an index in a triangular set.
+		/// </summary>
+		/// <exception cref="ArgumentOutOfRangeException">Must be at least zero.</exception>
+		public static int Reverse(int n)
+		{
+			if (n < 0) throw new ArgumentOutOfRangeException(nameof(n), n, "Must be at least zero.");
+			Contract.EndContractBlock();
+
+			return (int)ReverseInternal(n);
 		}
 
 		public static class Disperse
 		{
 
 			/**
-             * Increases the number an element based on it's index.
+             * Increases the repeat of an element based on its index.
+			 * 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, ...
              * @param source
              * @returns {Enumerable<T>}
              */
 			public static IEnumerable<T> Increasing<T>(IEnumerable<T> source)
-			{
-				int i = 0;
-				return source.SelectMany(
-					c => Enumerable.Repeat(c, Interlocked.Increment(ref i)));
-			}
+				=> source.SelectMany((c, i) => Enumerable.Repeat(c, i));
 
 			/**
-             * Increases the count of each element for each index.
+             * Starting with the first item, increases the size of the loop of items until there is no more.
+			 * The quantity/total of an item is lessened as the loop spreads the items out as it progresses.
+			 * Given an entire set, the first item is represented the most and the last item is represented the least.
+			 * 1, 1, 2, 1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5, ...
              * @param source
              * @returns {Enumerable<T>}
              */
 			public static IEnumerable<T> Decreasing<T>(IEnumerable<T> source)
 			{
 				var s = source.Memoize();
-				int i = 0;
-				return s.SelectMany(
-					c => s.Take(Interlocked.Increment(ref i)));
+				return s.SelectMany((c, i) => s.Take(i));
 			}
+
+			/**
+			 * Effectively the same as Disperse.Increasing but in reverse.
+			 * Given a fininte set, calling Disperse.Increasing(sourse).Reverse() could produce the desired result as well.
+			 * 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5
+			 * @param source
+			 * @returns {Enumerable<T>}
+			 */
+			public static IEnumerable<T> Descending<T>(IReadOnlyList<T> source)
+				=> source.SelectMany((c, i) => Enumerable.Repeat(c, source.Count - i));
 		}
 	}
 
---
+++ TriangularIndexer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace Open.Arithmetic
+{
+	public class TriangularIndexer<T> : IReadOnlyList<T>
+	{
+		public readonly IReadOnlyList<T> Source;
+		public readonly bool IsDescending;
+
+		public TriangularIndexer(IReadOnlyList<T> source, bool descending = false)
+		{
+			Source = source ?? throw new ArgumentNullException(nameof(source));
+			if (source.Count > Triangular.MaxInt32)
+				throw new ArgumentException($"Source collection is too large to be indexed.  Max size: {Triangular.MaxInt32}", nameof(source));
+			Contract.EndContractBlock();
+
+			IsDescending = descending;
+		}
+
+		public T this[int index]
+			=> IsDescending
+			? Source[Source.Count - Triangular.Reverse(Count - index)]
+			: Source[Triangular.Reverse(index)];
+
+		public int Count
+			=> Triangular.ForwardInt(Source.Count);
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			var source = IsDescending
+				? Triangular.Disperse.Descending(Source)
+				: Triangular.Disperse.Increasing(Source);
+
+			foreach (var e in source)
+				yield return e;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+			=> GetEnumerator();
+	}
+
+	public static class TriangularIndexer
+	{
+		public static TriangularIndexer<T> Increasing<T>(IReadOnlyList<T> source)
+			=> new TriangularIndexer<T>(source);
+		public static TriangularIndexer<T> Decreasing<T>(IReadOnlyList<T> source)
+			=> new TriangularIndexer<T>(source, true);
+	}
+}
